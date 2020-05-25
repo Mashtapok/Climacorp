0c977b1cc29ed77035ac29e0de7231b632e1f6bd
Stepan <gowxking@gmail.com>
2020-05-25 14:33:43 +0300

    first

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+new fullpage("#fullpage", {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    anchors: ["main", "services, order-now, about-us,contancts"],
+  });
+
+  //methods
+  fullpage_api.setAllowScrolling(true);
